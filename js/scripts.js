f23cc0df0f01da4d70a326678a312d1ff82a6ff2
jboz <julienboz@gmail.com>
2017-07-27 00:04:13 +0200

    update content

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,7 @@
-// Empty JS for your own code to be here
+$(window).bind('scroll', function () {
+    if ($(window).scrollTop() > 200) {
+        $('header').addClass('fixed');
+    } else {
+        $('header').removeClass('fixed');
+    }
+});
