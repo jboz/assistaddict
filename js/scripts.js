105ad7dfed46f181b7df6f746747becfba154fc3
jboz <julienboz@gmail.com>
2023-04-01 15:00:45 +0200

    feature/upgrade bootstrap

---
+++ js/scripts.js
@@ -0,0 +1,61 @@
+$(window).bind("scroll", function () {
+  if ($(window).scrollTop() > 200) {
+    $("header").addClass("fixed");
+  } else {
+    $("header").removeClass("fixed");
+  }
+});
+
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function (event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $("html, body").animate(
+          {
+            scrollTop: target.offset().top - 271,
+          },
+          1000,
+          function () {
+            // Callback after animation
+            // Must change focus!
+            var $target = $(target);
+            $target.focus();
+            if ($target.is(":focus")) {
+              // Checking if the target was focused
+              return false;
+            } else {
+              $target.attr("tabindex", "-1"); // Adding tabindex for elements not focusable
+              $target.focus(); // Set focus again
+            }
+          }
+        );
+      }
+    }
+  });
+
+$("#navigation a").click(function () {
+  $("#navbar-menu-items").removeClass("in");
+});
+
+function mail() {
+  document.location.href = "mailto:contact" + "@" + "assistaddict.com";
+}
+
+function sms() {
+  document.location.href = "sms:" + "+33" + "764071910";
+}
